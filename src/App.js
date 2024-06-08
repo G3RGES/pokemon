df8799665723a555b750918ba31940d8f2b830d2
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-08 15:29:57 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,25 +3,59 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 import axios from "axios";
+import PokemonList from "./components/PokemonList";
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [previousPageUrl, setPreviousPageUrl] = useState();
 
   useEffect(() => {
+    let cancel;
+
+    setLoading(true);
     axios
-      .get("https://pokeapi.co/api/v2/pokemon")
+      .get(currentPageUrl, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
       .then((res) => {
         // console.log(res);//* TESTING
         setPokemon(res.data.results.map((poke) => poke.name));
+        setLoading(false);
+        setNextPageUrl(res.data.next);
+        setPreviousPageUrl(res.data.previous);
       })
       .catch((err) => {
         console.log(err);
+        setLoading(false);
       });
-  }, []);
+
+    return () => {
+      cancel();
+    };
+  }, [currentPageUrl]);
 
   // console.log(pokemon); //* TESTING
 
-  return <div className="App">APP</div>;
+  if (loading) return "Loading...";
+
+  const nextPage = () => {
+    setCurrentPageUrl(nextPageUrl);
+  };
+
+  const prevPage = () => {
+    setCurrentPageUrl(previousPageUrl);
+  };
+
+  return (
+    <>
+      <PokemonList pokemon={pokemon} />
+    </>
+  );
 }
 
 export default App;
