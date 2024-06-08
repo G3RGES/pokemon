773f0464edc94b0da2a991cafb8a2acc83d8937d
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-08 15:50:57 +0300

    Update Pagination.jsx

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -5,6 +5,10 @@ const Pagination = ({ prevPage, nextPage, previousPageUrl, nextPageUrl }) => {
     <div>
       {nextPageUrl && <button onClick={nextPage}>Next</button>}
       {previousPageUrl && <button onClick={prevPage}>Previous</button>}
+
+      {/* //* AS SEEN IN VIDEO BUT DOESN'T WORK (MINE WORKS) */}
+      {/* {nextPage && <button onClick={nextPage}>Next</button>}
+      {prevPage && <button onClick={prevPage}>Previous</button>} */}
     </div>
   );
 };
