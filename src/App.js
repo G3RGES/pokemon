9114794b28606d0743f6702668b46061070c3874
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-08 14:28:59 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState, useEffect } from "react";
+
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
+      // console.log(res);//* TESTING
+      setPokemon(res.data.results.map((poke) => poke.name));
+    });
+  }, []);
+
+  // console.log(pokemon); //* TESTING
+
+  return <div className="App">APP</div>;
 }
 
 export default App;
